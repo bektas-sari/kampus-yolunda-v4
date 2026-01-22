b285f0085952eca58eb1ad83e9de550bc3b75a18
Bektas Sari <bektas.sari@gmail.com>
2026-01-22 08:58:41 +0300

    FIX: System restored to standard config, dependencies updated

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -3,10 +3,3 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-
-    def ready(self):
-        # Django baslarken bizim ozel admin dosyamizi zorla yukle
-        try:
-            import api.admin_fix
-        except ImportError:
-            pass
